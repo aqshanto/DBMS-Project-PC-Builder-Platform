49ab7456c660d2ab37d239555eb4f6a18dd66306
Md Abdul Quym Shanto <quym241-15-053@diu.edu.bd>
2026-03-05 15:53:33 +0600

    Update Query.sql

--- Database/Query.sql
+++ Database/Query.sql
@@ -31,3 +31,32 @@ WHERE c.product_id = 'CPU002'
   AND r.product_id = 'RAM001';
 
 -- (Eta run kore dekho, ager query te jekhane DDR4 motherboard gulo (jemon 'Z690 UD AX') eshechilo, ebar segulo automatically bad pore shudhu DDR5 gulo ashbe!)
+
+
+-- Ebar Cholo Magic Ta Test Kori!
+-- Tumi prothome kono ekta product er dam update korbe, ebang tarpor log table ta check korbe. Nicher query duto ekta ekta kore run kore dekho:
+-- Step 1: Dam Update koro (Admin action):
+-- Dhoro 'Core i9-14900K' (CPU001) er dam ekhon $600 ase. Amra dam komiye $550 kore dicchi.
+UPDATE PRODUCTS 
+SET price = 550.00 
+WHERE product_id = 'CPU001';
+
+-- Step 2: Log Table Check koro (Automatic result):
+-- Ebar eita run kore dekho, tumi kintu log table e kichu insert koro nai, kintu trigger auto kaj koreche!
+
+SELECT 
+    p.name AS 'Product Name',
+    l.old_price AS 'Old Price',
+    l.new_price AS 'New Price',
+    l.change_date AS 'Date Changed'
+FROM PRICE_HISTORY_LOG l
+JOIN PRODUCTS p ON l.product_id = p.product_id;
+-- (Eta run korle dekhbe magic er moto log table e record toiri hoye geche je CPU001 er dam 600 theke 550 hoyeche!)
+
+-- Ei code gulo run kore view toiri korar por, tumi just nicher ei ek line er query gulo run kore dekho. Dekhbe khub sundor table akare pura PC build ta show korche!
+-- Budget Gaming PC dekhar jonno:
+
+SELECT * FROM View_Budget_Gaming_PC;
+-- Office PC dekhar jonno:
+SELECT * FROM View_Office_PC;
+-- (Eta jokhon sir ke dekhabe, tokhon explain korbe je "Sir, frontend e bar bar complex join query na chaliye amra ekta Virtual Table (View) toiri kore rekhechi, jate loading time onek fast hoy.")
