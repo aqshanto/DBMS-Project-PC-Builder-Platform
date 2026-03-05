b63a9dfc2159bfaddcdc676d2ecb43e256540a51
Md Abdul Quym Shanto <quym241-15-053@diu.edu.bd>
2026-03-05 15:01:11 +0600

    Create tables for RAM, GPUs, PSUs, and more
    Added tables for RAM, GPUs, casings, PSUs, storage, and coolers with appropriate foreign key constraints.

--- Database/01_schema.sql
+++ Database/01_schema.sql
@@ -44,3 +44,60 @@ CREATE TABLE MOTHERBOARDS (
         ON UPDATE CASCADE 
         ON DELETE CASCADE
 );
+
+CREATE TABLE RAMS (
+    product_id VARCHAR(30) PRIMARY KEY,
+    ram_type VARCHAR(20) NOT NULL, -- e.g., DDR4, DDR5
+    capacity_gb INT NOT NULL,
+    speed_mhz INT NOT NULL,
+    FOREIGN KEY (product_id) REFERENCES PRODUCTS(product_id) 
+        ON UPDATE CASCADE 
+        ON DELETE CASCADE
+);
+
+CREATE TABLE GPUS (
+    product_id VARCHAR(30) PRIMARY KEY,
+    length_mm DECIMAL(6, 2) NOT NULL, -- Casing clearance check korar jonno
+    tdp_watt DECIMAL(6, 2) NOT NULL,  -- Total wattage hisab korar jonno
+    passmark_score INT,
+    FOREIGN KEY (product_id) REFERENCES PRODUCTS(product_id) 
+        ON UPDATE CASCADE 
+        ON DELETE CASCADE
+);
+
+CREATE TABLE CASINGS (
+    product_id VARCHAR(30) PRIMARY KEY,
+    supported_form_factors VARCHAR(255) NOT NULL, -- e.g., 'ATX, Micro-ATX, Mini-ITX'
+    max_gpu_length_mm DECIMAL(6, 2) NOT NULL,
+    FOREIGN KEY (product_id) REFERENCES PRODUCTS(product_id) 
+        ON UPDATE CASCADE 
+        ON DELETE CASCADE
+);
+
+CREATE TABLE PSUS (
+    product_id VARCHAR(30) PRIMARY KEY,
+    wattage DECIMAL(6, 2) NOT NULL,
+    form_factor VARCHAR(50) DEFAULT 'ATX',
+    FOREIGN KEY (product_id) REFERENCES PRODUCTS(product_id) 
+        ON UPDATE CASCADE 
+        ON DELETE CASCADE
+);
+
+CREATE TABLE STORAGE (
+    product_id VARCHAR(30) PRIMARY KEY,
+    form_factor VARCHAR(50) NOT NULL,    -- e.g., 'M.2 2280', '2.5 inch', '3.5 inch'
+    interface_type VARCHAR(50) NOT NULL, -- e.g., 'NVMe PCIe 4.0', 'SATA III'
+    capacity_gb INT NOT NULL,
+    FOREIGN KEY (product_id) REFERENCES PRODUCTS(product_id) 
+        ON UPDATE CASCADE 
+        ON DELETE CASCADE
+);
+
+CREATE TABLE COOLERS (
+    product_id VARCHAR(30) PRIMARY KEY,
+    cooler_type VARCHAR(50) NOT NULL,        -- e.g., 'Air Cooler', 'AIO Liquid Cooler'
+    supported_sockets VARCHAR(255) NOT NULL, -- e.g., 'LGA1700, AM5, AM4'
+    FOREIGN KEY (product_id) REFERENCES PRODUCTS(product_id) 
+        ON UPDATE CASCADE 
+        ON DELETE CASCADE
+);
